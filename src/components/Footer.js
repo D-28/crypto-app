6ebeee35fe0d33bbfa14e24d44c66507425d80a6
D-28 <dsmirror28@gmail.com>
2022-02-09 22:14:40 -0500

    cleanup nextjs bloat | add Footer component | refactor

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      Powered by the{" "}
+      <a href="https://thegraph.com/hosted-service/subgraph/uniswap/uniswap-v2">
+        Uniswap V2 Subgraph
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <main>
+          <h1>Crypto App</h1>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
